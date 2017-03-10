f5218392e3318c40ab0af2d19c12bbc2b822ed02
fake_leo <ins227@gmail.com>
2017-03-10 15:21:34 +0300

    Minor syntax update

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ def start(message):
     bot.send_message(message.chat.id, 'Иди-ка ты нахуй, {}!'.format(message.from_user.first_name))
 
 @bot.message_handler(commands=['help'])
-def start(message):
+def help_me(message):
     bot.send_message(message.chat.id, 'Помощи он, блять, захотел!')
 
 @bot.message_handler(func=lambda message: True)
@@ -24,7 +24,7 @@ def echo_all(message):
 
     # turn off
     if active and ('angrybot' in text.lower() or 'бот' in text.lower()) and ('заткнись' in text.lower() or 'усни' in text.lower()):
-        bot.send_message(message.chat.id, 'Ой, ну всё!')        
+        bot.send_message(message.chat.id, 'Ой, ну всё!')
         active = False
 
     # turn on
@@ -34,15 +34,15 @@ def echo_all(message):
 
     # hi
     elif active and ('angrybot' in text.lower() or 'бот' in text.lower()) and ('здоров' in text.lower() or 'привет' in text.lower()):
-        
+
         hi_list = ['Привет, {}!'.format(user), 'Приветствую тебя, {}!'.format(user), 'Здоров, {}!'.format(user), 'Здравствуй, {}!'.format(user),
                     'Привет!', 'Привет-привет!', 'Моё почтение, {}!'.format(user)]
-        
+
         bot.send_message(message.chat.id, random.choice(hi_list))
 
     # how r u
     elif active and ('angrybot' in text.lower() or 'бот' in text.lower()) and ('как дела?' in text.lower()):
-        
+
         status_list = ['Нормально, {}!'.format(user), 'Ты знаешь, {}? Хорошо!'.format(user), 'Норм',
                        'Нормально', 'Хорошо!', 'Заебись!', 'Отлично!', 'Так себе!', 'Ничо так!', 'Да не очень...',
                        '48 :)', 'В целом, пойдет!', 'Великолепно!', 'Потрясающе!']
@@ -60,7 +60,6 @@ def echo_all(message):
                     '{} - {}!'.format(user, random.choice(['хам', 'редиска', 'хамло', 'нахал, недалёкий человек'])),
                     'Хуй поспоришь!', 'Приехали...']
             res = random.choice(bot_answer_list)
-
 
         elif 'блять' in text.lower():
             if randint(0, 10) >= 5:
@@ -81,7 +80,7 @@ def echo_all(message):
 
         elif text[-1] == '?':  # question
             if randint(0,10) >= 8:
-                
+
                 qst_list = ['Тебе это зачем?', 'Я не по курсам', 'Не, не знаю', 'Сам-то что думаешь?', 'Ага! Щас...', 'Я не уверен в ответе',
                    'Это зависит не от меня', 'Не в этот раз!', 'А это интересно, да?', 'Что ж, поглядим...', 'Надо подумать',
                    'Вот я тоже мучаюсь этим вопросом!', 'А ведь и правда?', 'Что скажете, мужики?']
@@ -91,8 +90,5 @@ def echo_all(message):
             bot.send_message(message.chat.id, res)
 
 
-
-
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
-
+    bot.polling(none_stop=True)
